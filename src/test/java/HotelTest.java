9e7b59751033ed01e36c792583be5a7dc214104e
Laura Nagy <ngylaura@gmail.com>
2018-10-10 11:26:44 +0100

    refactors HotelTest

--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -16,23 +16,26 @@ public class HotelTest {
         bedroom = new Bedroom("Pentmoon suite", 2);
         confRoom = new ConferenceRoom("The Quorum", 20);
         diningRoom = new DiningRoom("The Granary", 25);
+        hotel.addBedroom(bedroom);
+        hotel.addConferenceRoom(confRoom);
+        hotel.addDineRoom(diningRoom);
     }
 
     @Test
     public void hasRooms() {
-        assertEquals(0, hotel.getBedrooms().size());
-        assertEquals(0, hotel.getConfRooms().size());
-        assertEquals(0, hotel.getDineRooms().size());
+        assertEquals(1, hotel.getBedrooms().size());
+        assertEquals(1, hotel.getConfRooms().size());
+        assertEquals(1, hotel.getDineRooms().size());
     }
 
     @Test
     public void addRooms() {
         hotel.addBedroom(bedroom);
-        assertEquals(1, hotel.getBedrooms().size());
+        assertEquals(2, hotel.getBedrooms().size());
         hotel.addConferenceRoom(confRoom);
-        assertEquals(1, hotel.getConfRooms().size());
+        assertEquals(2, hotel.getConfRooms().size());
         hotel.addDineRoom(diningRoom);
-        assertEquals(1, hotel.getDineRooms().size());
+        assertEquals(2, hotel.getDineRooms().size());
     }
 
 //    @Test
