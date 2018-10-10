140c8fc760951a0077f2888d48f5fee7565826db
Laura Nagy <ngylaura@gmail.com>
2018-10-10 10:51:03 +0100

    tests ConferenceRoom class

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class ConferenceRoom {
+
+    private String name;
+    private int capacity;
+    private ArrayList<Guest> guests;
+
+    public ConferenceRoom(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+        this.guests = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public ArrayList getGuests() {
+        return this.guests;
+    }
 }
+
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+    ConferenceRoom confRoom;
+
+    @Before
+    public void before() {
+        confRoom = new ConferenceRoom("The Quorum", 20);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("The Quorum", confRoom.getName());
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(20, confRoom.getCapacity());
+    }
+
+    @Test
+    public void hasGuests() {
+        assertEquals(0, confRoom.getGuests().size());
+    }
 }
